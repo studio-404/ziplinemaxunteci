ba9ea9096923c9221b48e4863173acc53eff2c6f
gio <hosting@404.ge>
2021-02-07 08:32:49 +0000

    homepage done

--- up.sh
+++ up.sh
@@ -1,3 +1,3 @@
-sass /home/alttool/domains/zipline.alttool.com/public_html/_markup/assets/scss/ge.scss /home/alttool/domains/zipline.alttool.com/public_html/_markup/assets/css/ge.css;
+sass /home/alttool/domains/zipline.alttool.com/public_html/_markup/assets/scss/ge.scss /home/alttool/domains/zipline.alttool.com/public_html/_markup/assets/css/ge.css --style compressed;
 
-sass /home/alttool/domains/zipline.alttool.com/public_html/_markup/assets/scss/main.scss /home/alttool/domains/zipline.alttool.com/public_html/_markup/assets/css/main.css
+sass /home/alttool/domains/zipline.alttool.com/public_html/_markup/assets/scss/main.scss /home/alttool/domains/zipline.alttool.com/public_html/_markup/assets/css/main.css --style compressed;
