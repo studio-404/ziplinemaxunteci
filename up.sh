541761fafd65ff854cb13e901013e70e021e6fc2
gio <hosting@404.ge>
2021-02-16 10:14:52 +0000

    save before notebook

--- up.sh
+++ up.sh
@@ -1,3 +1,5 @@
 sass /home/alttool/domains/zipline.alttool.com/public_html/_markup/assets/scss/ge.scss /home/alttool/domains/zipline.alttool.com/public_html/_markup/assets/css/ge.css --style compressed;
 
+sass /home/alttool/domains/zipline.alttool.com/public_html/_markup/assets/scss/responsive_ge.scss /home/alttool/domains/zipline.alttool.com/public_html/_markup/assets/css/responsive_ge.css --style compressed;
+
 sass /home/alttool/domains/zipline.alttool.com/public_html/_markup/assets/scss/main.scss /home/alttool/domains/zipline.alttool.com/public_html/_markup/assets/css/main.css --style compressed;
